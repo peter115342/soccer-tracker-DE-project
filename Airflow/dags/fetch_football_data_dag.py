cb532d7048710d281a61834374abe7f1a989e58f
peter115342 <pmuzslay8@gmail.com>
2024-10-18 17:50:36 +0200

    fixes

--- Airflow/dags/fetch_football_data_dag.py
+++ Airflow/dags/fetch_football_data_dag.py
@@ -5,14 +5,12 @@ from datetime import timedelta
 import requests
 from typing import Dict, Any
 import os
-from dotenv import load_dotenv
 
 default_args: Dict[str, Any] = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'email': "pmuzslay8@gmail.com",
-    'email_on_failure': True,
-    'email_on_retry': True,
+    'email_on_failure': False,
+    'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
