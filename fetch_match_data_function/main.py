78b1959ccb596ff5e0cd31619df1ccf6049f36d1
peter115342 <pmuzslay8@gmail.com>
2024-10-27 10:54:30 +0100

    duplicate matches fix, changing Discord notification color

--- fetch_match_data_function/main.py
+++ fetch_match_data_function/main.py
@@ -1,9 +1,10 @@
 from typing import List, Dict, Any
 from flask import Request
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 import json
 import os
+import logging
 
 from utils.api_helpers_match import fetch_matches_by_date_range
 from utils.data_processing import process_match_data
@@ -16,23 +17,29 @@ def fetch_football_data(request: Request):
     """
     try:
         date_from, date_to = get_date_range(request)
+        logging.info(f"Fetching matches between {date_from} and {date_to}.")
         matches = fetch_matches_by_date_range(date_from, date_to)
 
         if not matches:
-            message = f"No matches found between {date_from} and {date_to}."
-            send_discord_notification("⚠️ Fetch Football Data: No Matches", message)
+            message = f"No finished matches found on {date_from}."
+            send_discord_notification("⚠️ Fetch Football Data: No Matches", message, 16776960)  # Yellow
             return message, 200
 
-        processed_matches = process_match_data(matches, date_from)
+        processed_matches = process_match_data(matches)
         result = insert_matches(processed_matches)
 
-        success_message = f"Inserted data for {len(processed_matches)} matches into BigQuery."
-        send_discord_notification("✅ Fetch Football Data: Success", success_message)
-        return result, 200
+        if result['inserted_count'] == 0:
+            success_message = f"No new matches to insert into BigQuery. Skipped {result['skipped_count']} matches that already existed."
+        else:
+            success_message = f"Inserted data for {result['inserted_count']} new matches into BigQuery. Skipped {result['skipped_count']} matches that already existed."
+
+        send_discord_notification("✅ Fetch Football Data: Success", success_message, 65280)  # Green
+        return success_message, 200
     except Exception as e:
         error_message = f"An error occurred: {str(e)}"
-        send_discord_notification("❌ Fetch Football Data: Failure", error_message)
-        raise
+        send_discord_notification("❌ Fetch Football Data: Failure", error_message, 16711680)  # Red
+        logging.exception(error_message)
+        return error_message, 500
 
 def get_date_range(request: Request) -> tuple[str, str]:
     request_args = request.args
@@ -40,34 +47,36 @@ def get_date_range(request: Request) -> tuple[str, str]:
         date_from = request_args['dateFrom']
         date_to = request_args['dateTo']
     else:
-        yesterday = datetime.now()
-        date_from = yesterday.strftime('%Y-%m-%d')
+        today = datetime.now()
+        date_from = today.strftime('%Y-%m-%d')
         date_to = date_from
     return date_from, date_to
 
-def insert_matches(processed_matches: List[Dict[str, Any]]) -> str:
-    insert_data_into_bigquery('match_data', processed_matches)
-    return f"Inserted data for {len(processed_matches)} matches into BigQuery."
+def insert_matches(processed_matches: List[Dict[str, Any]]) -> Dict[str, int]:
+    result = insert_data_into_bigquery('match_data', processed_matches)
+    return result
 
-def send_discord_notification(title: str, message: str):
+def send_discord_notification(title: str, message: str, color: int):
     webhook_url = os.environ.get('DISCORD_WEBHOOK_URL')
     if not webhook_url:
         print("Discord webhook URL not set.")
         return
+
     discord_data = {
         "content": None,
         "embeds": [
             {
                 "title": title,
                 "description": message,
-                "color": 65280 if "Success" in title else (16776960 if "No Fixtures" in title else 16711680)
+                "color": color
             }
         ]
     }
+
     headers = {
         "Content-Type": "application/json"
     }
+
     response = requests.post(webhook_url, data=json.dumps(discord_data), headers=headers)
     if response.status_code != 204:
         print(f"Failed to send Discord notification: {response.status_code}, {response.text}")
-
