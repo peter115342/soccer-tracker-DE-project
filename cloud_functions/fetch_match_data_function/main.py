bc70652b784a41619c7fd8756efd9d97ef0df541
peter115342 <pmuzslay8@gmail.com>
2024-11-20 22:46:29 +0100

    only sending message when new matches

--- cloud_functions/fetch_match_data_function/main.py
+++ cloud_functions/fetch_match_data_function/main.py
@@ -23,10 +23,26 @@ def fetch_football_data(event, context):
         processed_matches = []
 
         if not matches:
-            message = f"No new matches found on date {date_from}"
+            message = f"No new matches found on date {date_from}. Skipping conversion step."
             logging.info(message)
             send_discord_notification("ℹ️ Fetch Match Data: No New Matches", message, 16776960)
-            
+            return "No new matches to process.", 200
+
+        for match in matches:
+            try:
+                match_id = match['id']
+                save_to_gcs(match, match_id)
+                processed_matches.append(match)
+                new_matches += 1
+            except Exception as e:
+                error_count += 1
+                logging.error(f"Error processing match ID {match_id}: {e}")
+
+        success_message = f"Fetched {new_matches} new matches. Errors: {error_count}. Triggering conversion step."
+        logging.info(success_message)
+        send_discord_notification("✅ Fetch Match Data: Success", success_message, 65280)
+
+        if new_matches > 0:
             publisher = pubsub_v1.PublisherClient()
             topic_path = publisher.topic_path(os.environ['GCP_PROJECT_ID'], 'convert_to_parquet_topic')
 
@@ -42,38 +58,6 @@ def fetch_football_data(event, context):
 
             publish_result = future.result()
             logging.info(f"Published trigger message to convert_to_parquet_topic with ID: {publish_result}")
-            
-            return "No new matches to process.", 200
-
-        for match in matches:
-            try:
-                match_id = match['id']
-                save_to_gcs(match, match_id)
-                processed_matches.append(match)
-                new_matches += 1
-            except Exception as e:
-                error_count += 1
-                logging.error(f"Error processing match ID {match_id}: {e}")
-
-        success_message = f"Fetched {new_matches} new matches. Errors: {error_count}"
-        logging.info(success_message)
-        send_discord_notification("✅ Fetch Match Data: Success", success_message, 65280)
-
-        publisher = pubsub_v1.PublisherClient()
-        topic_path = publisher.topic_path(os.environ['GCP_PROJECT_ID'], 'convert_to_parquet_topic')
-
-        publish_data = {
-            "action": "convert_matches"
-        }
-
-        future = publisher.publish(
-            topic_path,
-            data=json.dumps(publish_data).encode('utf-8'),
-            timestamp=datetime.now().isoformat()
-        )
-
-        publish_result = future.result()
-        logging.info(f"Published trigger message to convert_to_parquet_topic with ID: {publish_result}")
 
         return "Process completed.", 200
 
