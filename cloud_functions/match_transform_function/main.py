75ffef8c5de6815ae38fc512b838a3632d21823c
peter115342 <pmuzslay8@gmail.com>
2024-12-04 21:12:07 +0100

    fix: simplify dataform release config listing

--- cloud_functions/match_transform_function/main.py
+++ cloud_functions/match_transform_function/main.py
@@ -20,7 +20,7 @@ def trigger_dataform_workflow():
         repository=repository_id,
     )
 
-    release_configs = client.list_release_configs(parent=repository, page_size=1)
+    release_configs = client.list_release_configs(parent=repository)
     latest_release_config = next(iter(release_configs))
 
     workflow_invocation = dataform_v1beta1.WorkflowInvocation(
