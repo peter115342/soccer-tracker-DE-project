757b2306187a834790cd6f8da6877c21576b8f6d
peter115342 <pmuzslay8@gmail.com>
2025-01-27 17:58:01 +0100

    fix : added default value for vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,9 @@
 variable "project_id" {
   description = "GCP Project ID"
   type        = string
+  default     = ""
+  sensitive = true
+
 }
 
 variable "region" {
@@ -12,50 +15,62 @@ variable "region" {
 variable "bucket_name" {
   description = "GCS Bucket Name"
   type        = string
+  default     = ""
+
 }
 
 variable "api_football_key" {
   description = "API Football Key"
   type        = string
   sensitive   = true
+  default     = ""
+
 }
 
 variable "discord_webhook_url" {
   description = "Discord Webhook URL"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "google_maps_api_key" {
   description = "Google Maps API Key"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "reddit_client_id" {
   description = "Reddit Client ID"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "reddit_client_secret" {
   description = "Reddit Client Secret"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "dataform_repository" {
   description = "Dataform Repository Name"
   type        = string
+  default     = ""
 }
 
 variable "dataform_workspace" {
   description = "Dataform Workspace Name"
   type        = string
+  default     = ""
 }
 
 variable "service_account_email" {
   description = "Service Account Email for Pub/Sub push authentication"
   type        = string
+  sensitive = true
+  default     = ""
 }
 
