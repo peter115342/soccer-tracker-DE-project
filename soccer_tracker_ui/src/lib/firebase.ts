2c2537c688a0e9811dcea12720319b2de56670c9
peter115342 <pmuzslay8@gmail.com>
2025-02-16 11:16:50 +0100

    fix: trying with hardcoded projectid

--- soccer_tracker_ui/src/lib/firebase.ts
+++ soccer_tracker_ui/src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-    authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
+   // apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+    authDomain: "vigilant-shell-435820-r2.firebaseapp.com",
+    projectId: "vigilant-shell-435820-r2",
+    storageBucket: "vigilant-shell-435820-r2.firebasestorage.app",
+   // messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    //appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
 };
 
 
