b319c8119b781b31f857bbc6a1ace67fbad6d353
peter115342 <pmuzslay8@gmail.com>
2025-02-16 10:52:19 +0100

    feat : add  firebase env vars to Github Actions

--- soccer_tracker_ui/src/lib/firebase.ts
+++ soccer_tracker_ui/src/lib/firebase.ts
@@ -1,9 +1,27 @@
-import { initializeApp } from 'firebase/app';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    projectId: "vigilant-shell-435820",
+    apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+    authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
 };
 
+
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+
+
+
